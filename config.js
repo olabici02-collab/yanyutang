969dffc853c0c4d6f5e6f8aa162211fb620b7e3b
olabici02-collab <olabici02@gmail.com>
2026-04-07 14:45:49 +0800

    Update config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = "https:///vobsxsjabdxmkojphizm.supabase.co";  // ← 替换这里
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvYnN4c2phYmR4bWtvanBoaXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDQ3OTgsImV4cCI6MjA5MDY4MDc5OH0.EYGaENUoKhIxYoCkv7f7poO8zLuzzEM_iPaZuk5YL9s"; // ← 替换这里
+ 
+// 图片存储 Bucket 名称（与你在 Supabase Storage 创建的一致）
+const STORAGE_BUCKET = "product-images"; 
